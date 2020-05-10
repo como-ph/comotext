a80cbad838a71b2c9d10dab723f3b7f60d9fd3a3
Ernest Guevarra <ernest@guevarra.io>
2020-05-10 18:19:55 +0100

    update get_press_release function

--- R/02-get_press_release.R
+++ R/02-get_press_release.R
@@ -7,12 +7,15 @@
 #'   Default is \url{https://www.doh.gov.ph/press-releases}
 #' @param pages A vector of page numbers corresponding to the page panel
 #'   containing the press release link. Default is 1:25. Press releases only go
-#'   up to page 25.
+#'   up to page 25. Values higher than 25 will error.
 #'
-#' @return A tibble of 2 columns: 1) press release title; and, 2) date of press
-#'   release.
+#' @return A tibble of 2 columns: 1) press release title; 2) press release
+#'   unique identifier; and, 3) date of press release.
 #'
 #' @examples
+#' ## Get press release URLs for first page of press release panel. The base
+#' ## argument has been specified using non-SSL version of base URL to prevent
+#' ## erroring for those with proxy server connections (i.e., in Travis tests)
 #' get_pr_url(base = "http://www.doh.gov.ph/press-releases",
 #'            pages = 1)
 #'
@@ -23,8 +26,15 @@
 
 get_pr_url <- function(base = "https://www.doh.gov.ph/press-releases",
                        pages = 1:25) {
+  ## Check that pages goes up to 25 only
+  if(max(pages) > 25) {
+    stop("The current maximum pages for press releases in the DoH website
+         is 25. Please try again.", call. = TRUE)
+  }
+
   ## Concatenating vectors
   prURL <- NULL
+  prID <- NULL
   prDate <- NULL
 
   ## Cycle through pages
@@ -32,6 +42,7 @@ get_pr_url <- function(base = "https://www.doh.gov.ph/press-releases",
     wp <- paste(base, "?page=", i - 1, sep = "")
     if(i == 1) wp <- base
 
+    ## Extract and process press release relative links
     href <- xml2::read_html(x = wp) %>%
       rvest::html_nodes(css = ".view-content .views-field-title .field-content a") %>%
       rvest::html_attr(name = "href")
@@ -39,22 +50,56 @@ get_pr_url <- function(base = "https://www.doh.gov.ph/press-releases",
     href <- stringr::str_subset(string = href, pattern = "press-release|node")
     href <- stringr::str_subset(string = href, pattern = "20343|19904", negate = TRUE)
 
+    ## Generate unique identifiers for each press release
+    id <- stringr::str_split_fixed(string = href, pattern = "/", n = 3)[ , 3]
+    id <- id %>%
+      stringr::str_remove_all(pattern = "-") %>%
+      stringr::str_extract(pattern = "[a-zA-Z]{8}") %>%
+      stringr::str_to_upper() %>%
+      stringr::str_split(pattern = "", simplify = TRUE)
+
+    id <- matrix(data = match(id, LETTERS[1:26]),
+                 nrow = nrow(id),
+                 ncol = 8,
+                 byrow = FALSE)
+
+    hrefID <- NULL
+
+    for(j in 1:nrow(id)) {
+      if(all(is.na(id[j, ]))) {
+        hrefID <- c(hrefID,
+                    stringr::str_extract(string = href[j],
+                                         pattern = "[0-9]{4}|[0-9]{5}|[0-9]{6}|[0-9]{7}|[0-9]{8}"))
+      } else {
+        hrefID <- c(hrefID, paste(id[j, ], collapse = ""))
+      }
+    }
+
+    hrefID <- stringr::str_extract(string = hrefID,
+                                   pattern = "[0-9]{4}|[0-9]{5}|[0-9]{6}|[0-9]{7}|[0-9]{8}")
+
+    ## Extract and process press release issue date
     hrefDate <- xml2::read_html(x = wp) %>%
       rvest::html_nodes(css = ".view-content .content-time") %>%
       rvest::html_text()
 
     hrefDate <- hrefDate[1:length(href)]
 
-    ##
+    ## Concatenate url, id and date
     prURL <- c(prURL, href)
+    prID <- c(prID, hrefID)
     prDate <- c(prDate, hrefDate)
   }
 
-  ##
+  ## Convert prDate to date format
   prDate <- lubridate::mdy(prDate)
 
-  ##
+  ## Convert prID to numeric
+  prID <- as.numeric(prID)
+
+  ## Create tibble
   pr <- tibble::tibble(data.frame(url = prURL,
+                                  id = prID,
                                   date = prDate,
                                   stringsAsFactors = FALSE))
 
@@ -70,18 +115,19 @@ get_pr_url <- function(base = "https://www.doh.gov.ph/press-releases",
 #'
 #' @param base Base URL for press releases in the Department of Health website.
 #'   Default is \url{https://www.doh.gov.ph}
-#' @param url of press release to extract text from
-#' @param date Date press release was issued. Should be in <YYYY-MM-DD> format.
+#' @param df A data.frame created using \code{get_pr_url} providing values for
+#'   relative URL of press release/s, unique identifier of press release and,
+#'   date of issue of press release.
 #'
 #' @return A tibble containing text of the press release with additional
-#'   information on line number, type of text and date of press release.
+#'   information on line number, type of text, unique identifier and date of
+#'   press release.
 #'
 #' @examples
 #' prURL <- get_pr_url(base = "http://www.doh.gov.ph/press-releases",
 #'                     pages = 1)
 #' get_press_release(base = "http://www.doh.gov.ph",
-#'                   url = prURL$url[1],
-#'                   date = prURL$date[1])
+#'                   df = prURL[1, ])
 #'
 #' @export
 #'
@@ -90,10 +136,9 @@ get_pr_url <- function(base = "https://www.doh.gov.ph/press-releases",
 ################################################################################
 
 get_press_release <- function(base = "https://www.doh.gov.ph",
-                              url,
-                              date) {
+                              df) {
   ## Form URL
-  url <- paste(base, url, sep = "")
+  url <- paste(base, df$url, sep = "")
 
   ## Extract text from URL
   z <- xml2::read_html(x = url) %>%
@@ -117,8 +162,8 @@ get_press_release <- function(base = "https://www.doh.gov.ph",
   pressRelease <- data.frame(linenumber = 1:length(pressRelease),
                              text = pressRelease,
                              type = "press release",
-                             id = NA,
-                             date = date,
+                             id = df$id,
+                             date = df$date,
                              stringsAsFactors = FALSE)
 
   ## Convert pressRelease to tibble
