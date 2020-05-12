8f12d6a4075b6281344034dcc030a9b2106fa722
Ernest Guevarra <ernest@guevarra.io>
2020-05-12 18:00:23 +0100

    create new function to pull table of IATF resolutions

---
+++ R/01-get_iatf.R
@@ -0,0 +1,42 @@
+################################################################################
+#
+#'
+#' Get list of links to IATF resolutions
+#'
+#' @param base URL to the IATF resolutions webpage
+#'
+#' @return A tibble of links to all the current IATF resolutions
+#'
+#' @examples
+#' get_iatf_links()
+#'
+#' @export
+#'
+#
+################################################################################
+
+get_iatf_links <- function(base = "https://www.doh.gov.ph/COVID-19/IATF-Resolutions") {
+  ## Get page table
+  xx <- xml2::read_html(base) %>%
+    rvest::html_nodes(css = ".panel .view-content .views-table") %>%
+    rvest::html_table()
+
+  ## Get href links per resolution
+  yy <- xml2::read_html(base) %>%
+    rvest::html_nodes(css = ".panel .view-content .views-field a") %>%
+    rvest::html_attr(name = "href")
+
+  ## Extract Resolution number from links
+  id <- stringr::str_extract(string = yy, pattern = "[0-9]+")
+
+  ## Add links id to links
+  yy <- data.frame(id, link = yy)
+
+  ## Merge links with page table
+  linkTable <- merge(xx[[1]], yy, by.x = "Resolution No.", by.y = "id")
+  names(linkTable) <- c("id", "title", "date", "link")
+
+  ## Return
+  return(linkTable)
+}
+
